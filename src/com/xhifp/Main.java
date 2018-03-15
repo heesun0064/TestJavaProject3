a7e5b24255f5f1612ebe3849c90257a9cdd133d4
spiderman <spiderman@xhift.co.kr>
2018-03-15 17:18:42 +0900

    merged two version

--- src/com/xhifp/Main.java
+++ src/com/xhifp/Main.java
@@ -5,9 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("spiderman 1");
         System.out.println("spiderman 2");
-        System.out.println("spiderman 33");
         System.out.println("deadpool 1");
         System.out.println("deadpool 2");
-        System.out.println("deadpool 3");
+        System.out.println("deadpool 33");
     }
 }
