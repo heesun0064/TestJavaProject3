57cfdad3515b53737cf1a46f370ed84393298516
Heesun Kim <heesun0064@gmail.com>
2018-03-15 17:11:11 +0900

    Update Main.java

--- src/com/xhifp/Main.java
+++ src/com/xhifp/Main.java
@@ -3,12 +3,8 @@ package com.xhifp;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("spiderman 1");
-        System.out.println("spiderman 2");
-        System.out.println("spiderman 3");
-        System.out.println("spiderman 4");
-        System.out.println("spiderman 5");
         System.out.println("deadpool 1");
         System.out.println("deadpool 2");
+        System.out.println("deadpool 3");
     }
 }
