d8beca85acbed319b588ae7366df3cf77a791d18
spiderman <spiderman@xhift.co.kr>
2018-03-15 17:34:45 +0900

    Merge branch 'master' of https://github.com/heesun0064/TestJavaProject3
    # Conflicts:
    #	src/com/xhifp/Main.java

--- src/com/xhifp/Main.java
+++ src/com/xhifp/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("deadpool 1");
-        System.out.println("deadpool 2");
-        System.out.println("deadpool 3");
+        System.out.println("spiderman 1");
     }
 }
