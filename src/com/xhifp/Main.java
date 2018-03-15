8b2f037ee08f67c48ea8d34455e75aaee46a870b
dambi095 <37395389+dambi095@users.noreply.github.com>
2018-03-15 17:30:02 +0900

    2삭제

--- src/com/xhifp/Main.java
+++ src/com/xhifp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("deadpool 1");
-        System.out.println("deadpool 22");
+        System.out.println("deadpool 2");
         System.out.println("deadpool 3");
     }
 }
